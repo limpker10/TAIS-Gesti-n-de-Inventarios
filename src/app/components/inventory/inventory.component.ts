0b72502c61687e36c8745e3692d3fe9093db6a92
JOSE MIGUEL CANO VILCAPAZA <jcanov@unsa.edu.pe>
2024-11-25 18:52:18 -0500

    agregar configuración de rutas y actualizar componente principal

---
+++ src/app/components/inventory/inventory.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+@Component({
+  selector: 'app-inventory',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './inventory.component.html',
+  styleUrl: './inventory.component.css'
+})
+export class InventoryComponent {
+
+}
